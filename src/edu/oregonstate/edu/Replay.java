b95192d3b0dffcc9113b0915742f866f53426657
Michael Hilton <michael.hilton@gmail.com>
2013-10-24 15:43:00 -0700

    added some testing framework

---
+++ src/edu/oregonstate/edu/Replay.java
@@ -0,0 +1,11 @@
+package edu.oregonstate.edu;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelhilton
+ * Date: 10/24/13
+ * Time: 3:39 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Replay {
+}
---
+++ tests/edu/oregonstate/edu/ReplayTest.java
@@ -0,0 +1,17 @@
+package edu.oregonstate.edu;
+
+import org.junit.Before;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: michaelhilton
+ * Date: 10/24/13
+ * Time: 3:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ReplayTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+}
